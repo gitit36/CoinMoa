11a153b89510911b74adfcaf8ffb0b6089a44733
David Sangjin Lee <sl5583@nyu.edu>
2026-02-22 16:50:32 +0900

    Delete coinTax directory

--- coinTax/archive/Lighter 복사본/collect_usdt_krw_minute_price.py
+++
@@ -1,7 +0,0 @@
-"""Backward-compatible wrapper for minute price collector."""
-from scripts.collect_usdt_krw_minute_price import *  # noqa: F401,F403
-from scripts.collect_usdt_krw_minute_price import main
-
-
-if __name__ == "__main__":
-    main()
--- coinTax/archive/Lighter 복사본/csv_analysis.py
+++
@@ -1,2 +0,0 @@
-"""Backward-compatible wrapper for csv analysis utilities."""
-from scripts.csv_analysis import *  # noqa: F401,F403
